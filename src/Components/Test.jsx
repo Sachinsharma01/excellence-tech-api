bdac00b19add8a27902045f96d5c8792f5f62830
sachin sharma <sachin2252sharma@gmail.com>
2021-09-27 00:25:10 +0530

    fixed bugs

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Users from "./Users";
 import "./Test.css";
 import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Test = () => {
@@ -11,9 +12,9 @@ const Test = () => {
   useEffect(() => {
     async function fetchData() {
       const data = await axios(
-        value <= 5
-          ? `https://reqres.in/api/users?page=${value}`
-          : `https://reqres.in/api/users?page=1`
+        // value <= 5
+        `https://reqres.in/api/users?page=${value}`
+        // : `https://reqres.in/api/users?page=1`
       );
       console.log(`useEffect with value ${value}`);
       setUsers(data.data.data);
@@ -21,7 +22,11 @@ const Test = () => {
     fetchData();
   }, [value]);
   const buttonHandler = () => {
-    setValue((val) => val + 1);
+    if (value > 5) {
+      setValue(0);
+    } else {
+      setValue((val) => val + 1);
+    }
   };
   console.log(users);
   return (
@@ -35,13 +40,20 @@ const Test = () => {
       >
         Next page
       </Button>
-      <div className="container">
-        <div className="users">
-          {users.map((user) => (
-            <Users user={user} key={user.name} />
-          ))}
+      {!(users.length === 0) ? (
+        <div className="container">
+          <div className="users">
+            {users.map((user) => (
+              <Users user={user} key={user.name} />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <Typography variant="h3" sx={{ textAlign: "center" }}>
+          No data to Show<br />
+          Click Next
+        </Typography>
+      )}
     </>
   );
 };
